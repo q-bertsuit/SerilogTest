3ebd4c519a3759ce930b9ccd54f43352f0b183b2
Magnus Dale <md@sekal.com>
2021-01-18 08:25:42 +0100

    Serilog test

---
+++ LibrarySdk/LoggerInstaller.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Serilog.Configuration;
+namespace LibrarySdk
+{
+    static public class LoggerInstaller
+    {
+        static public Serilog.Core.Logger Install()
+        {
+            //SelfLog.Enable(Console.Error);
+
+            string logFileName = "MyLog.log";
+
+            var config = new LoggerConfiguration()
+                                .ReadFrom.AppSettings()
+                                //.WriteTo.File(logFileName, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 50000)
+                                .CreateLogger();
+
+            Log.Logger = config;
+
+            return config;
+        }
+
+    }
+}
---
+++ SerilogTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+using System;
+using Library;
+    
+namespace SerilogTest
+{
+    class Program
+    {
+        private static readonly ILogger Logger = Log.ForContext(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            LoggerInstaller.Install();
+
+            Logger.Information("It works!");
+
+            Console.ReadLine();
+        }
+    }
+}
